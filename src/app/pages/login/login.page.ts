9a302c270c8b06fc12a88b0069ffb118866c88c3
YhonVivas <yohnvivas16@gmail.com>
2025-03-18 08:36:32 -0300

    ultimos cambios

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonHeader, 
+  IonToolbar, 
+  IonContent, 
+  IonTitle, 
+  IonRow, 
+  IonCol, 
+  IonGrid,
+  IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonContent, IonTitle, CommonModule, FormsModule, IonRow, IonCol, IonGrid, IonButton]
 })
 export class LoginPage implements OnInit {
 
